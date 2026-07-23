df647129534094156273705febf8bb8aeb80c8f8
mottakinrahat <rahatchowdhury661@gmail.com>
2026-07-23 11:28:21 +0600

    workingDeplyoment

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Server running on: http://localhost:${port}/api/v1`);
   console.log(`📄 Swagger docs at:   http://localhost:${port}/api/v1/docs`);
